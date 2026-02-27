14178bd81e09970883bd23a67b5c010c429aca64
Huligesh Malka <huligeshmalka@gmail.com>
2026-02-27 22:10:07 +0530

    added policy pages

--- src/index.js
+++ src/index.js
@@ -1,28 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
-/* ✅ AUTH PROVIDER */
-import { AuthProvider } from "./context/AuthProvider";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter
-      future={{
-        v7_startTransition: true,
-        v7_relativeSplatPath: true,
-      }}
-    >
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter 
+    future={{ 
+      v7_startTransition: true, 
+      v7_relativeSplatPath: true 
+    }}
+  >
+    <App />
+  </BrowserRouter>
 );
 
 reportWebVitals();
