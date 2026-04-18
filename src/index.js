5060e5a6b3f7c95fc2c6830f3546a59fb31243f0
Huligesh Malka <huligeshmalka@gmail.com>
2026-04-18 12:54:50 +0530

    Fix UPI QR compatibility

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration"; // ✅ ADD
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
