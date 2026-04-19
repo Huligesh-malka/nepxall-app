40a8317ec507c032dbf388930ec8e428ef4d4b7e
Huligesh Malka <huligeshmalka@gmail.com>
2026-04-19 14:56:59 +0530

    Fix UPI QR compatibility

---
+++ src/pages/PublicHome.js
@@ -0,0 +1,9 @@
+// pages/PublicHome.jsx
+export default function PublicHome() {
+  return (
+    <div>
+      <h1>Find Your Perfect Stay</h1>
+      {/* your PG listing component */}
+    </div>
+  );
+}
