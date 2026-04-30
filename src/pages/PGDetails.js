28d94195c0547b70a75862fcf274e47b86be3a88
Huligesh Malka <huligeshmalka@gmail.com>
2026-04-30 17:25:02 +0530

    Fix UPI QR compatibility

--- src/pages/PGDetails.js
+++ src/pages/PGDetails.js
@@ -2259,7 +2259,7 @@ export default function PGDetails() {
 )}
 
 {/* AGREEMENT */}
-{(pg.agreement_mandatory === true ||
+{Number(pg.agreement_mandatory === true ||
   pg.agreement_mandatory === "true") && (
   <RuleItem 
     icon="📄" 
@@ -2270,7 +2270,7 @@ export default function PGDetails() {
 )}
 
 {/* ID PROOF */}
-{(pg.id_proof_mandatory === true ||
+{Number(pg.id_proof_mandatory === true ||
   pg.id_proof_mandatory === "true") && (
   <RuleItem 
     icon="🆔" 
